a9be0cc48c40b704c7970968458db3631c8116e2
EricSchles <ericschles@gmail.com>
2014-11-04 16:11:34 -0800

    first commit

---
+++ sample.py
@@ -0,0 +1,11 @@
+class Foo:
+    def __init__(self,first,second):
+        self.first = first
+        self.second = second
+
+    def adder(self):
+        return self.first + self.second
+
+    def multipler(self):
+        return self.first * self.second
+
